5be20b394ef0d5c4584494acf12de35d5e75e8e2
PatchOsodo <osodopatch@yahoo.com>
2022-09-12 15:58:36 +0000

    Check Betty coding style always

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,26 +6,26 @@
  */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 100; i++) /*print first two digit combo*/
 	{
 		for (j = 0; j < 100; j++) /*print second digit combo*/
+		{
+			if (i < j)
 			{
-				if (i < j)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar (' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar((i / 10) + 48);
-					putchar((i % 10) + 48);
-					putchar (' ');
-					putchar((j / 10) + 48);
-					putchar((j % 10) + 48);
-					if (i != 98 || j!= 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
