e4f3ce45443be81bc920880640dd0f5984e7fdc0
PatchOsodo <osodopatch@yahoo.com>
2022-09-21 17:15:42 +0000

    A fresh start

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
-#ifndef _MAIN_H
+#ifndef _MAIN_H_
 #define _MAIN_H_
+/**_strcat - concatenates src to dest
+ * @dest: Destination string
+ * @src: source string
+ * Return: dest, the destination character
+ */
+char *_strcat(char *dest, char *src);
 
-
-#endif /*_MAIN_H_*/
+#endif
