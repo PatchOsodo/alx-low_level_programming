702471e7c6ae7fff83b30b29cbd94de1514fe848
PatchOsodo <osodopatch@yahoo.com>
2022-09-21 17:25:20 +0000

    Adding _putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-/**_strcat - concatenates src to dest
- * @dest: Destination string
- * @src: source string
- * Return: dest, the destination character
- */
+int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 
 #endif
