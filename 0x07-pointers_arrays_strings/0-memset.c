02667c4f52dbd338b5d4a0cbe2da89e79f19dcad
PatchOsodo <osodopatch@yahoo.com>
2022-09-26 18:33:43 +0000

    Check your counters

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  * _memset - fills the first n bytes of memory area pointed to by s with b
+ *
  * @s: memory area to be filled
  * @b: constant bytes to fill up the memory spaces
  * @n: number of bytes to be filled
+ *
  * Return: pointer to memory area 's'
  */
 
@@ -15,7 +17,7 @@ char *_memset(char *s, char b, unsigned int n)
 	if (i > 0)
 	{
 		for (j = 0; j < i; j++)
-			s[i] = b;
+			s[j] = b;
 	}
 
 	return (s);
